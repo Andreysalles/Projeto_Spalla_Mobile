a2d8d1ed40369f91fef7091eb433d0efafcd5f92
Andreysalles <andreyspereira@hotmail.com>
2022-10-27 08:39:27 -0300

    Att 27/10

--- lib/telas/home_widgets/home_content.dart
+++ lib/telas/home_widgets/home_content.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:spallamobile/telas/home_widgets/home_list_item.dart';
 
+import "home_list_model.dart";
 import "package:flutter/material.dart";
 
 class HomePageContent extends StatefulWidget {
@@ -10,6 +12,9 @@ class HomePageContent extends StatefulWidget {
 }
 
 class _HomePageContentState extends State<HomePageContent> {
+  List<HomeListModel> listMockedList = [
+    HomeListModel(title: "Home - Spalla", assetIcon: "assets/Guilherme.png"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -38,6 +43,9 @@ class _HomePageContentState extends State<HomePageContent> {
               padding: EdgeInsets.only(
                 bottom: 32,
               ),
+
+              //Header
+
               child: CircleAvatar(
                 radius: 72,
                 backgroundColor: Color.fromARGB(255, 85, 90, 95),
@@ -54,10 +62,11 @@ class _HomePageContentState extends State<HomePageContent> {
                   color: Color.fromARGB(255, 94, 158, 255),
                   fontSize: 20,
                   fontWeight: FontWeight.w600),
-            )
+            ),
           ],
         ),
       ),
     );
+    // ignore: dead_code
   }
 }
---
+++ lib/telas/home_widgets/home_list_model.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+class HomeListModel {
+  dynamic icon;
+  dynamic assetIcon;
+  dynamic title;
+
+  HomeListModel({this.icon, this.title, this.assetIcon});
+}
